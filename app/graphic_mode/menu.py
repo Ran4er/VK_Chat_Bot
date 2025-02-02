2356055fd454e20db7124d476daf94ce84ca1146
Ran4er <dd@bloom.com>
2025-02-03 02:58:36 +0300

    feat: change interface and add functional

--- app/graphic_mode/menu.py
+++ app/graphic_mode/menu.py
@@ -1,18 +1,18 @@
 import subprocess
 import sys
+import time
 
 try:
     import curses
 
 except ImportError:
-    print("vk_api не найден. Установка...")
+    print("curses не найден. Установка...")
     subprocess.check_call([sys.executable, "-m", "pip", "install", "windows_curses"])
 
-    import vk_api
+    import curses
 
 
 def draw_button(stdscr, y, x, text, selected):
-    # Цвета для выбранной и невыбранной кнопки
     if selected:
         color = curses.color_pair(1)
         border_color = curses.color_pair(1)
@@ -20,7 +20,6 @@ def draw_button(stdscr, y, x, text, selected):
         color = curses.color_pair(2)
         border_color = curses.color_pair(2)
 
-    # Рисуем рамку кнопки
     stdscr.attron(border_color)
     stdscr.addstr(y, x, "+" + "-" * (len(text) + 2) + "+")
     stdscr.addstr(y + 1, x, "| " + text + " |")
@@ -28,42 +27,87 @@ def draw_button(stdscr, y, x, text, selected):
     stdscr.attroff(border_color)
 
 
+def run_bot():
+    process = subprocess.Popen([sys.executable, "../main.py"])
+    return process
+
+
+def draw_progress_bar(stdscr, progress, y, x, width=30):
+    fill_width = int(progress * width)
+    bar = "█" * fill_width + "-" * (width - fill_width)
+    stdscr.addstr(y, x, f"[{bar}] {int(progress * 100)}%")
+
+
 def main(stdscr):
     curses.start_color()
-    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)  # Выделенная кнопка
-    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE) # Обычная кнопка
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)  # Активная кнопка
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE) # Неактивная кнопка
 
     curses.curs_set(0)
     current_selection = 0
-    buttons = ["Запуск", "Выход"]
+    buttons = ["Запустить", "Выход"]
+    bot_process = None
 
     while True:
         stdscr.clear()
         height, width = stdscr.getmaxyx()
 
-        # Центрируем кнопки
+        # Центрирование
         start_y = (height - (len(buttons) * 4)) // 2
-        start_x = (width - (max(len(b) for b in buttons) + 4)) // 2
+        start_x = (width - 20) // 2
+
 
         for idx, button in enumerate(buttons):
             y = start_y + idx * 4
             draw_button(stdscr, y, start_x, button, idx == current_selection)
+            '''color = curses.color_pair(1) if idx == current_selection else curses.color_pair(2)
+            stdscr.attron(color)
+            stdscr.addstr(y, start_x, f"  {button}  ")
+            stdscr.attroff(color)'''
 
         stdscr.refresh()
         key = stdscr.getch()
 
-        # Управление выбором
         if key == curses.KEY_UP and current_selection > 0:
             current_selection -= 1
         elif key == curses.KEY_DOWN and current_selection < len(buttons) - 1:
             current_selection += 1
         elif key in [curses.KEY_ENTER, 10, 13]:
-            if buttons[current_selection] == "Запуск":
+            if buttons[current_selection] == "Запустить":
                 stdscr.clear()
-                stdscr.addstr(height // 2, width // 2 - 8, "Программа запущена!")
+                stdscr.addstr(height // 2 - 2, start_x, "Запуск бота...")
+                for i in range(1, 11):
+                    draw_progress_bar(stdscr, i / 10, height // 2, start_x)
+                    stdscr.refresh()
+                    time.sleep(0.2)
+
+                bot_process = run_bot()
+
+                stdscr.clear()
+                stdscr.addstr(height // 2, start_x, "Бот запущен!")
+                stdscr.addstr(height // 2 + 2, start_x-(len("Нажмите 'Остановить' для выхода")//4), "Нажмите 'Остановить' для выхода")
                 stdscr.refresh()
-                stdscr.getch()
+
+                stop_selection = 0
+                while True:
+                    stop_button = "Остановить"
+                    color = curses.color_pair(1) if stop_selection == 0 else curses.color_pair(2)
+                    stdscr.attron(color)
+                    stdscr.addstr(height // 2 + 4, start_x, f"  {stop_button}  ")
+                    stdscr.attroff(color)
+                    stdscr.refresh()
+
+                    key = stdscr.getch()
+                    if key in [curses.KEY_ENTER, 10, 13]:
+                        if bot_process:
+                            bot_process.terminate()
+                            bot_process.wait()
+                        break
+
             elif buttons[current_selection] == "Выход":
+                if bot_process:
+                    bot_process.terminate()
+                    bot_process.wait()
                 break
 
 curses.wrapper(main)
